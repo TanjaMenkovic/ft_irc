cc09d272fa30e95835e2ccda7f7ae7ad86fe4780
tvalimak <tvalimak@student.hive.fi>
2024-11-18 17:25:04 +0200

    server testing

--- includes/irc.hpp
+++ includes/irc.hpp
@@ -1,8 +1,21 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <signal.h>
 
 typedef struct s_input
 {
     int port;
     std::string password;
 }   t_input;
+
+typedef struct s_sockaddr_in
+{
+
+} t_sockaddr_in;
