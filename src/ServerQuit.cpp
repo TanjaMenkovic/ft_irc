8008c11a617e161a76573999f6fd37eec8722f9f
tvalimak <tvalimak@student.hive.fi>
2024-12-10 13:27:03 +0200

    rework of quit command

--- src/ServerQuit.cpp
+++ src/ServerQuit.cpp
@@ -3,30 +3,30 @@
 namespace irc 
 {
 
-// void Server::quit(int client_fd, const std::string reason, std::map<std::string, irc::Channel> &channels) {
-//     std::string message;
-//     const std::string& user_nickname = users[client_fd].getNickname(); // Get the user's nickname
+void Server::quit(int client_fd, const std::string reason, std::map<std::string, irc::Channel> &channels) {
+    std::string message;
+    const std::string& user_nickname = users[client_fd].getNickname(); // Get the user's nickname
 
-//     // Remove user from all channels
-//     for (std::map<std::string, irc::Channel>::iterator it = channels.begin(); it != channels.end(); ++it) {
-//         irc::Channel& channel = it->second;
+    // Remove user from all channels
+    for (std::map<std::string, irc::Channel>::iterator it = channels.begin(); it != channels.end(); ++it) {
+        irc::Channel& channel = it->second;
 
-//         // Check if the user is in the channel
-//         const auto& users_map = channel.getUsers();
-//         if (users_map.find(user_nickname) != users_map.end()) {
-//             channel.removeUser(client_fd); // Remove the user from the channel
+        // Check if the user is in the channel
+        const auto& users_map = channel.getUsers();  // recreate the missing functions into ServerUser.cpp
+        if (users_map.find(user_nickname) != users_map.end()) {
+            channel.removeUser(client_fd); // Remove the user from the channel, recreate the missing functions into ServerUser.cpp
 
-//             // Notify remaining users in the channel
-//             message = RPL_QUIT(users[client_fd].getUsername(), users[client_fd].getNickname(), reason);
-//             send_to_joined_channels(client_fd, message);
-//         }
-//     }
+            // Notify remaining users in the channel
+            message = RPL_QUIT(users[client_fd].getUsername(), users[client_fd].getNickname(), reason);
+            send_to_joined_channels(client_fd, message);
+        }
+    }
 
-//     // Log the quit event
-//     std::cout << "User " << user_nickname << " disconnected: " << reason << std::endl;
+    // Log the quit event
+    std::cout << "User " << user_nickname << " disconnected: " << reason << std::endl;
 
-//     // Remove the user from the server's user list
-//     users.erase(client_fd);
-// }
+    // Remove the user from the server's user list
+    users.erase(client_fd);
+}
 
 } // namespace irc
