51d27a6d714e723c0fbd2ee66308bd7297c6181f
tvalimak <tvalimak@student.hive.fi>
2024-12-11 11:42:26 +0200

    work on topic command

--- src/ServerTopic.cpp
+++ src/ServerTopic.cpp
@@ -19,11 +19,20 @@ namespace irc
 */
 
 // If client uses the topic command while connected to an server but it hasnt joined any channels, the command needs to be provided
-// with the channel's name in form of #<channelname>
+// with the channel's name in form of #<channelname> in order to return a topic of specific channel.
 
 // First check if the topic command is used inside a channel, if it is then it can be ran without parameters
 // and it will just return the current topic of the channel if there is any.
 
+// User has to be an operator of the channel in order to change its topic. 
+// If user is not an operator of the channel it tries to change topic in, messages in following format will be displayed:
+// #channel1 You do not have access to change the topic on this channel
+
+// Tested on irc.freenode.net
+// command /topic lol given on a channel results in client sending:
+// << TOPIC #channel1 :lol
+// server responded with:
+// >> :tvalimak_!~tvalimak@freenode-ebn.s3h.4nuk5f.IP TOPIC #channel1 :lol
 
 void Server::topic(int client_fd, std::vector<std::string> tokens) {
 
