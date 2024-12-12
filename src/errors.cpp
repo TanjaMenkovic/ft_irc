c09b83c25ce9aaaf9171e994fa243977725e4639
Tanja Menkovic <tmenkovi@c2r2p6.hive.fi>
2024-12-12 14:06:33 +0200

    errors

--- src/errors.cpp
+++ src/errors.cpp
@@ -11,7 +11,7 @@ bool isValidPort(const std::string& str)
             return false;
     }
 
-    if (str.length() > 1 && str[0] == '0')
+    if ((str.length() > 1 && str[0] == '0') || str.length() > 5)
         return false;
 
     if (std::stoi(str) < 0 || std::stoi(str) > 65535)
